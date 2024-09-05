06849f36b9abe8c9f8856befc9bca67129b1003f
Richárd Kovács <richardkovacs.it@gmail.com>
2024-09-05 18:06:18 +0200

    refactor: rename to replichat

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    library: 'chat',
+    library: 'replichat',
     libraryTarget: 'umd'
   },
   module: {
