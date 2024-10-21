6ae2b2a0b9b1ec9e444f1a82f246ef616e1f364a
Richárd Kovács <richardkovacs.it@gmail.com>
2024-10-21 21:17:51 +0200

    feat(config): load from server

--- src/initialize.jsx
+++ src/initialize.jsx
@@ -2,30 +2,41 @@ import React from "react";
 import Chat from "./chat";
 import { createRoot } from "react-dom/client";
 
-export function initializeChatbot() {
-  const config = getConfig();
+export async function initializeChatbot() {
+  const config = await getConfig();
   createBotRoot(config);
 }
 
-function getConfig() {
+async function getConfig() {
   const chatbotScript = document.querySelector("script[data-askthing-bot]");
 
   if (!chatbotScript) {
-    console.error("Chatbot script not found");
+    console.error("AskThing script not found");
     return;
   }
 
-  let config = {};
-  const configAttr = chatbotScript.getAttribute("data-askthing-config");
-  if (configAttr) {
-    try {
-      config = JSON.parse(configAttr);
-    } catch (e) {
-      console.error("Invalid chatbot configuration:", e);
-    }
+  const apiKey = chatbotScript.getAttribute("data-askthing-api-key");
+
+  if (!apiKey) {
+    console.error("AskThing API key not found");
+    return;
   }
 
-  return config;
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/bot/${apiKey}/config`
+  );
+
+  if (!response.ok) {
+    console.error("Failed to fetch AskThing configuration");
+    return;
+  }
+
+  const config = await response.json();
+
+  return {
+    ...config,
+    apiKey,
+  };
 }
 
 function createBotRoot(config) {
