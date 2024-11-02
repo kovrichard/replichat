9c00c3a9199384f58fe9f0c9795a35e5a5df143f
Richárd Kovács <richardkovacs.it@gmail.com>
2024-11-02 19:45:59 +0100

    feat: load api key from env locally

--- src/index.jsx
+++ src/index.jsx
@@ -5,18 +5,19 @@ import { createRoot } from 'react-dom/client';
 import { initializeChatbot } from './initialize';
 import { createId } from '@paralleldrive/cuid2';
 
-const config = {
-    title: 'Test Assistant',
-    initialMessages: [
-        {
-            id: createId(),
-            role: "assistant",
-            content: "Hello! How can I help you today?",
-        },
-    ],
-};
-
 if (process.env.NODE_ENV === 'development') {
+    const config = {
+        title: 'Test Assistant',
+        initialMessages: [
+            {
+                id: createId(),
+                role: "assistant",
+                content: "Hello! How can I help you today?",
+            },
+        ],
+        apiKey: process.env.ASKTHING_API_KEY,
+    };
+
     const domNode = document.getElementById('askthing-root');
     const root = createRoot(domNode);
     root.render(<Chat config={config} />);
